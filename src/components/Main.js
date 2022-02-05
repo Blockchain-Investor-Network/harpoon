28e40a150ddefee69bd5c85cb27e7394104a7d98
Carl Locigno <clocigno@gmail.com>
2022-02-05 13:22:59 -0500

    ERC20 Balance

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,18 @@
-import { useMoralis, useNativeBalance } from "react-moralis";
+import { useMoralis, useNativeBalance, useERC20Balances } from "react-moralis";
 
 export default function Main() {
     const { user } = useMoralis();
     const address = user.get("ethAddress");
-    const { getBalances, data: balance, nativeToken, error, isLoading } = useNativeBalance();
- 
+    const { data: balance } = useNativeBalance();
+    const { data: erc20balances } = useERC20Balances();
+    const formattedERC20balance = erc20balances[0].balance / 1000000000000000000;
 
     return (
         <div>
             <h1>Welcome {user.get("username")}</h1>
             <h1>{address}</h1>
             <h3>{balance.formatted}</h3>
+            <h3>{formattedERC20balance} {erc20balances[0].symbol}</h3>
         </div>
     );
 }
